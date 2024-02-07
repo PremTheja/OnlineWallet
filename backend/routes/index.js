1d5cf82df3ecb85227860cb22e3c72ae645a9760
PremTheja <premthejapamula@gmail.com>
2024-02-08 00:20:52 +0530

    added backend routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const express=require("express")
 const router=express.Router()
 
-
 const userRouter=require("./user")
 router.use("/user",userRouter)
+
+const accountRouter = require("./account");
+router.use("/account",accountRouter);
 
 
 
