b8c459768c7721bfc7f849a23a6ce857fe697769
PremTheja <premthejapamula@gmail.com>
2024-02-11 13:24:38 +0530

    updated Middleware and account.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,10 +4,9 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = require("../config");
 const bcrypt = require("bcrypt");
 //const { default: authMiddleware } = require("../middleware");
-const  { authMiddleware } = require("../middleware");
+const { authMiddleware } = require("../middleware");
 const express = require("express");
 const router = express.Router();
-
 
 const signupBody = zod.object({
   username: zod.string().email(),
@@ -44,14 +43,25 @@ router.post("/signup", async (req, res) => {
     });
 
     const userId = user._id;
+
+     //adding random amount in account
+     await Account.create({
+      userId,
+      balance: 1 + Math.random() * 100000
+  })
+
+
     const token = jwt.sign(
       {
         userId,
       },
-      JWT_SECRET
+      JWT_SECRET,
+      {
+        expiresIn: '1d' // Expires in 1 day
+      }
     );
 
-    res.status(200).json({
+  res.status(200).json({
       message: "User created successfully",
       token: token,
     });
@@ -69,101 +79,104 @@ const signinBody = zod.object({
 });
 
 router.post("/signin", async (req, res) => {
-    const { success } = signinBody.safeParse(req.body)
-    if (!success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
+  const { success } = signinBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(411).json({
+        message: "No such user exists",
+      });
     }
-    const {username , password} = req.body;
-
-    try{
-
-        const user = await User.findOne({username});
-
-        if(!user)
-        {
-            return res.status(411).json({
-                message: "No such user exists"
-            })
-        }
-        const passwordMatch = await bcrypt.compare(password, user.password); 
-        if(!passwordMatch)
-        {
-            return res.status(401).json({
-                message: "Invalid Credentials"
-            })
-        }
-        const userId = user._id;
-
-        const token = jwt.sign({
-            userId
-        }, JWT_SECRET);
-
-        res.status(200).json({
-            message:"Login successfull",
-            token:token
-        })
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      });
     }
-    catch (error) {
-        console.error("Error during signin:", error);
-        res.status(500).json({
-            message: "Internal server error",
-        });
-    }
-})
+    const userId = user._id;
+
+    const token = jwt.sign(
+      {
+        userId,
+      },
+      JWT_SECRET,
+    );
+
+    res.status(200).json({
+      message: "Login successfull",
+      token: token,
+    });
+  } catch (error) {
+    console.error("Error during signin:", error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+});
 //update
 
 const updateBody = zod.object({
-	password: zod.string().optional(),
-    firstName: zod.string().optional(),
-    lastName: zod.string().optional(),
-})
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
 
-router.put("/" , authMiddleware, async (req,res) => {
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
 
-    const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
 
-    if (!success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
-    }
+  await User.updateOne(req.body, {
+    _id: req.userId,
+  });
 
-    await User.updateOne(req.body, {
-        _id: req.userId
-    })
-
-    res.json({
-        message: "Updated successfully"
-    })
-
-})
+  res.json({
+    message: "Updated successfully",
+  });
+});
 
 router.get("/bulk", async (req, res) => {
-    const filter = req.query.filter || "";
+  const filter = req.query.filter || "";
 
-    const users = await User.find({
-        $or: [{
-            firstName: {
-                "$regex": filter
-            }
-        }, {
-            lastName: {
-                "$regex": filter
-            }
-        }]
-    })
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
 
-    res.json({
-        user: users.map(user => ({
-            username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            _id: user._id
-        }))
-    })
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
+});
+
+router.get("/verify" , authMiddleware , (req,res) => {
+  res.status(200).json({message:"User is logged in"});
 })
-
 
 module.exports = router;
